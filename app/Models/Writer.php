9cefa23c30c6c30a9a59858c8d1e4f4d1b5aae13
mnurullahsaglam <nurullahsl87@gmail.com>
2023-10-28 15:15:17 +0300

    add booksRead relation to Writer model

--- app/Models/Writer.php
+++ app/Models/Writer.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Traits\Slugger;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -26,5 +27,11 @@ class Writer extends Model
     public function books(): HasMany
     {
         return $this->hasMany(Book::class);
+    }
+
+    public function booksRead(): HasMany
+    {
+        return $this->hasMany(Book::class)
+            ->where('is_finished', true);
     }
 }
