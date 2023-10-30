e36d3bdd994e014d748bdda4ddd41ae2bd9a687b
mnurullahsaglam <nurullahsl87@gmail.com>
2023-10-31 00:00:52 +0300

    fix writer progress column

--- resources/views/tables/columns/progress-column.blade.php
+++ resources/views/tables/columns/progress-column.blade.php
@@ -1,11 +1,11 @@
-@if($getState())
+@if(! $getState() && $getState() !== 0)
+    <div class="text-gray-400 text-sm">
+        Henüz kitap eklenmemiş.
+    </div>
+@else
     <div class="progress-bar">
         <div class="progress-bar-value" style="width: {{ $getState() }}%;">
             %{{ $getState()}}
         </div>
-    </div>
-@else
-    <div class="text-gray-400 text-sm">
-        Henüz kitap eklenmemiş.
     </div>
 @endif
