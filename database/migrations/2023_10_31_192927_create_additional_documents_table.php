67b393995cb1b690ba9e407317ad306973a4fbe0
mnurullahsaglam <nurullahsl87@gmail.com>
2023-10-31 19:43:26 +0300

    create migration for additional documents

---
+++ database/migrations/2023_10_31_192927_create_additional_documents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Meeting;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('additional_documents', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(Meeting::class)->nullable()->constrained()->nullOnDelete();
+
+            $table->string('name');
+            $table->string('file');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('additional_documents');
+    }
+};
