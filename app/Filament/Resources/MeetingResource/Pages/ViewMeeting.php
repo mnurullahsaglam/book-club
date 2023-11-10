c950e8b5b3f9a825cc5ae836afc647f84701bc51
mnurullahsaglam <nurullahsl87@gmail.com>
2023-11-10 16:48:25 +0300

    add header action buttons (edit, delete and export) in view page

--- app/Filament/Resources/MeetingResource/Pages/ViewMeeting.php
+++ app/Filament/Resources/MeetingResource/Pages/ViewMeeting.php
@@ -3,10 +3,25 @@
 namespace App\Filament\Resources\MeetingResource\Pages;
 
 use App\Filament\Resources\MeetingResource;
-use Filament\Actions;
+use App\Models\Meeting;
+use Filament\Actions\Action;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewMeeting extends ViewRecord
 {
     protected static string $resource = MeetingResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            EditAction::make(),
+            DeleteAction::make(),
+            Action::make('export')
+                ->label('PDF\'e Aktar')
+                ->color('info')
+                ->url(fn(Meeting $record) => route('meetings.export.pdf', $record), true)
+        ];
+    }
 }
