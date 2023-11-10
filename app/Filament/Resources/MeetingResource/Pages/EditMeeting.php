071d98b8121c85e9cdc40a4532da5cb7a87434c6
mnurullahsaglam <nurullahsl87@gmail.com>
2023-11-10 16:47:44 +0300

    add ViewAction to edit page

--- app/Filament/Resources/MeetingResource/Pages/EditMeeting.php
+++ app/Filament/Resources/MeetingResource/Pages/EditMeeting.php
@@ -4,7 +4,8 @@ namespace App\Filament\Resources\MeetingResource\Pages;
 
 use App\Filament\Resources\MeetingResource;
 use App\Traits\FilamentRedirect;
-use Filament\Actions;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditMeeting extends EditRecord
@@ -16,7 +17,9 @@ class EditMeeting extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            ViewAction::make()
+                ->color('info'),
+            DeleteAction::make(),
         ];
     }
 }
