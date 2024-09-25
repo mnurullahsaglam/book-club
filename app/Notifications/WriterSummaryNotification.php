25787212ea28aa0d0e5301bedd6a62c979099b8c
mnurullahsaglam <nurullahsl87@gmail.com>
2024-09-25 12:18:02 +0300

    create writer summary notification

---
+++ app/Notifications/WriterSummaryNotification.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Writer;
+use App\Services\WriterSummaryService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\HtmlString;
+
+class WriterSummaryNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(private readonly Writer $writer)
+    {
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Yazar Özeti: ' . $this->writer->name)
+            ->greeting('Merhaba!')
+            ->line('Yazarın özeti aşağıdaki gibidir.')
+            ->line(new HtmlString((new WriterSummaryService($this->writer))->handle()))
+            ->salutation('Keyifli okumalar,');
+    }
+}
