64aa1fa69e4d0fa9a7f79e8d2b40a08b68a53ac6
mnurullahsaglam <nurullahsl87@gmail.com>
2024-10-15 17:04:13 +0300

    update presentation stream

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
 use App\Http\Controllers\MeetingPdfExportController;
-use App\Models\Presentation;
 
 Route::get('/meetings/{meeting}/export/pdf', MeetingPdfExportController::class)
     ->name('meetings.export.pdf')
-    ->middleware('auth');
-
-Route::get('/presentation/{presentation}', function (Presentation $presentation) {
-    return response()->file($presentation->file_url);
-})
-    ->name('presentation.show')
     ->middleware('auth');
