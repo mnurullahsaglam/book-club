f20ac23a121323605146ce5e5ad44ea58f7e10c7
mnurullahsaglam <nurullahsl87@gmail.com>
2024-10-16 00:19:59 +0300

    possible fix on presentations table

--- app/Filament/Resources/PresentationResource.php
+++ app/Filament/Resources/PresentationResource.php
@@ -103,8 +103,7 @@ class PresentationResource extends Resource
                     ->label('Dosyayı görüntüle')
                     ->icon('heroicon-o-eye')
                     ->color('info')
-                    ->url(fn ($record) => $record?->file_url)
-                    ->shouldOpenUrlInNewTab(),
+                    ->url(fn ($record) => $record?->file_url, true),
             ]);
     }
 
