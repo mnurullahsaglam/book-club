d7fcc07c31f5e3ba09d547a44d394c0494fc43bd
mnurullahsaglam <nurullahsl87@gmail.com>
2024-10-17 20:20:30 +0300

    refactor

--- app/Filament/Resources/AdditionalDocumentResource.php
+++ app/Filament/Resources/AdditionalDocumentResource.php
@@ -66,7 +66,8 @@ class AdditionalDocumentResource extends Resource
                     ->label('Dosyayı görüntüle')
                     ->icon('heroicon-o-eye')
                     ->color('info')
-                    ->url(fn ($record) => $record->file_url, true),
+                    ->url(fn ($record) => $record->file_url)
+                    ->openUrlInNewTab(),
                 EditAction::make(),
                 DeleteAction::make(),
             ])
