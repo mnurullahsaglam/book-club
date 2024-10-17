ea69008834d4c430c3388137b8632c2a9e0e2e28
mnurullahsaglam <nurullahsl87@gmail.com>
2024-10-17 20:13:47 +0300

    remove auth log from user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,12 +13,10 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Rappasoft\LaravelAuthenticationLog\Traits\AuthenticationLoggable;
 
 class User extends Authenticatable implements FilamentUser
 {
-    use AuthenticationLoggable,
-        HasApiTokens,
+    use HasApiTokens,
         HasFactory,
         Notifiable,
         Notifiable,
