8f7d9410df128e9f2fc3aa7be791463a06d9e8f8
mnurullahsaglam <mnurullahsaglam@users.noreply.github.com>
2024-10-27 14:08:40 +0000

    Fix styling

--- app/Models/Writer.php
+++ app/Models/Writer.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Writer extends Model
