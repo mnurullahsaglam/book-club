deeeeb287b886c3aa3606cba0e2cdcb09ea11331
mnurullahsaglam <nurullahsl87@gmail.com>
2025-04-04 23:44:17 +0300

    add updated meeting and user cases for Presentation

--- app/Models/Presentation.php
+++ app/Models/Presentation.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use App\Notifications\PresentationAssignedNotification;
+use App\Notifications\PresentationMeetingUpdatedNotification;
+use App\Notifications\PresentationRemovedNotification;
 use Filament\Notifications\Actions\Action;
 use Filament\Notifications\Notification;
 use Illuminate\Database\Eloquent\Builder;
@@ -60,6 +62,79 @@ class Presentation extends Model
 
             $user->notify(new PresentationAssignedNotification($presentation));
         });
+
+        self::updating(function (Presentation $presentation) {
+            if ($presentation->isDirty('meeting_id')) {
+                $oldMeeting = Meeting::find($presentation->getOriginal('meeting_id'));
+
+                $notificationText = "{$presentation->meeting->date->format('d/m/Y')} tarihindeki toplantı için sunumunuz güncellendi.";
+
+                $presentation->user->notify(
+                    Notification::make()
+                        ->title('Sunum Güncellendi')
+                        ->body($notificationText)
+                        ->icon('heroicon-o-document-check')
+                        ->iconColor('info')
+                        ->actions([
+                            Action::make('view')
+                                ->label('Görüntüle')
+                                ->button()
+                                ->url($presentation->file_url)
+                                ->openUrlInNewTab()
+                                ->visible(fn () => $presentation->file)
+                                ->markAsRead(),
+                        ])
+                        ->toDatabase()
+                );
+
+                $presentation->user->notify(new PresentationMeetingUpdatedNotification($presentation, $oldMeeting->date->format('d/m/Y')));
+            }
+
+            if ($presentation->isDirty('user_id')) {
+                $oldUser = User::find($presentation->getOriginal('user_id'));
+
+                $notificationText = "Sunumunuz yeni bir kullanıcıya atandı.";
+                $presentation->user->notify(
+                    Notification::make()
+                        ->title('Sunum Atandı')
+                        ->body($notificationText)
+                        ->icon('heroicon-o-document-check')
+                        ->iconColor('info')
+                        ->actions([
+                            Action::make('view')
+                                ->label('Görüntüle')
+                                ->button()
+                                ->url($presentation->file_url)
+                                ->openUrlInNewTab()
+                                ->visible(fn () => $presentation->file)
+                                ->markAsRead(),
+                        ])
+                        ->toDatabase()
+                );
+
+                $oldUser->notify(
+                    Notification::make()
+                        ->title('Sunum Başkasına Atandı')
+                        ->body($notificationText)
+                        ->icon('heroicon-o-document-check')
+                        ->iconColor('info')
+                        ->actions([
+                            Action::make('view')
+                                ->label('Görüntüle')
+                                ->button()
+                                ->url($presentation->file_url)
+                                ->openUrlInNewTab()
+                                ->visible(fn () => $presentation->file)
+                                ->markAsRead(),
+                        ])
+                        ->toDatabase()
+                );
+
+                $oldUser->notify(new PresentationRemovedNotification($presentation));
+
+                $presentation->user->notify(new PresentationAssignedNotification($presentation));
+            }
+        });
     }
 
     public function title(): Attribute
@@ -86,16 +161,16 @@ class Presentation extends Model
         );
     }
 
+    public function scopeOwner(Builder $query): Builder
+    {
+        return $query->where('user_id', auth()->id());
+    }
+
     protected function citation(): Attribute
     {
         return Attribute::make(
             get: fn () => $this->citation ?? $this->generateCitation(),
         );
-    }
-
-    public function scopeOwner(Builder $query): Builder
-    {
-        return $query->where('user_id', auth()->id());
     }
 
     private function generateCitation()
