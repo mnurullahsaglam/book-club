80d580345a5743b727240e8b83ea20f377e1ee59
mnurullahsaglam <nurullahsl87@gmail.com>
2025-04-04 23:39:35 +0300

    create PresentationRemovedNotification

---
+++ app/Notifications/PresentationRemovedNotification.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Presentation;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PresentationRemovedNotification extends Notification
+{
+    public function __construct(private readonly Presentation $presentation)
+    {
+        //
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Sunum Başkasına Atandı')
+            ->greeting('Merhaba!')
+            ->line('Önümüzdeki toplantı için size verilen sunum başkasına atandı.')
+            ->line("**{$this->presentation->citation}**")
+            ->line('Lütfen size başka sunumlar atanıp atanmadığını kontrol edin.')
+            ->salutation('Keyifli okumalar,');
+    }
+}
