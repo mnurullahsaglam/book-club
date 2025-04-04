bd2a7594590bbdff6cfb85213d0a18e53fa30a64
mnurullahsaglam <nurullahsl87@gmail.com>
2025-04-04 23:23:38 +0300

    fix lazy loading issue on local in meeting edit form

--- app/Filament/Resources/MeetingResource.php
+++ app/Filament/Resources/MeetingResource.php
@@ -139,7 +139,7 @@ class MeetingResource extends Resource
                 Section::make('Katılımcılar')
                     ->schema([
                         Repeater::make('meetingUsers')
-                            ->relationship('meetingUsers')
+                            ->relationship('meetingUsers', fn(Builder $query) => $query->with('user'))
                             ->hiddenLabel()
                             ->schema([
                                 Hidden::make('user_id')
