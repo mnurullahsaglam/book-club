cdb42712a752a3413eff603a25f1bc77091fc8a1
mnurullahsaglam <nurullahsl87@gmail.com>
2025-04-04 23:16:11 +0300

    re-enable model strictness rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //Model::shouldBeStrict(! app()->isProduction());
+        Model::shouldBeStrict(! app()->isProduction());
 
         CreateAction::configureUsing(function (CreateAction $action) {
             $action->icon('heroicon-o-plus');
